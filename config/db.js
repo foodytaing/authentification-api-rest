0037b629d81f844f5cd2a402109a82be90d9901a
foodytaing <foody.taing@gmail.com>
2021-01-24 13:39:16 +0100

    config

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USER_PW}@cluster0.it9bf.mongodb.net/test`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        }
+    )
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Failed to connect to MongoDB', err));
